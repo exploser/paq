484d5da1a10f89fb7eb54e91b5ab932b7bb7044d
Pavel Vasilev <explosere@ya.ru>
2015-11-27 12:03:00 +0300

    Moved big methods to libpaq.lua Added custom directory structure support

---
+++ src/libpaq.lua
@@ -0,0 +1,132 @@
+-- paq: An Unrestricted Github-Powered Package Manager for OpenOS
+-- author: exploser (Pavel Vasilev)
+-- exploser@exsdev.ru, explosere@ya.ru
+
+local VERSION = "0.1.2"
+local binname = "paq"
+local configpath = "/etc/" .. binname .. "/"
+local repositorypath = "/usr/" .. binname .. "/"
+local installpath = "/usr/bin/"
+
+local fs = require("filesystem")
+local internet = require("internet")
+local text = require("text")
+local serialization = require("serialization")
+local installed = {}
+
+local paq = {}
+
+function paq.usage()
+  io.write("Usage: " .. binname .. " command [argument]\n")
+  io.write("Commands:\n")
+  io.write(" install [user] paqname\t\tinstall package <paqname> from Github user <user>\n")
+  io.write(" if no user was specified, uses the author's repository (exploser)\n")
+  io.write(" version\t\t\tprint " .. binname .. " version\n")
+end
+
+function paq.init()
+  if fs.exists(configpath .. "paqs.tb") then
+    local f = io.open(configpath .. "paqs.tb", "r")
+    local data = f:read("*all")
+    installed = serialization.unserialize(data) or {}
+    f:close()
+  elseif not fs.exists(configpath) then
+    fs.makeDirectory(configpath)
+  end
+end
+
+function paq.get(paqname, authorname)
+  authorname = authorname or "exploser"
+  
+  if(installed[paqname]) then
+    if(installed[paqname].author == authorname) then
+      -- do something
+    end
+    io.write(string.format("Package %s:%s is already installed! Use '%s delete %s' to remove it.\n", installed[paqname].author, paqname, binname, paqname))
+  end
+  
+  local masterurl = "https://raw.githubusercontent.com/" .. authorname .. "/" .. paqname .. "/master/"
+  local infourl = masterurl .. "package.txt"
+  io.write(string.format("Retrieving %s...\n", infourl))
+  local result, response = pcall(internet.request, infourl)
+  
+  if result then
+    local str = ""
+    for chunk in response do
+      str = str .. chunk
+    end
+    local t = serialization.unserialize(str)
+    
+    if t then
+      io.write(str.."\n")
+      local repo = repositorypath .. paqname .. "/"
+      
+      io.write("Creating local directories...\n")
+      fs.makeDirectory(repo)
+      
+      installed[paqname] = {}
+      installed[paqname].files = {}
+      
+      for file, dest in pairs(t.files) do
+        --local index = string.find(file, "/[^/]*$") or 0
+        --local filename = string.sub(file, index + 1)
+        if(fs.exists(dest)) then
+          io.stderr:write(string.format("File %s already exists! Try removing it by hand. %s does not delete existing files if it didn't install them.\n", dest, binname))
+          installed[paqname] = nil
+          return
+        end
+        
+        os.execute(string.format("wget %s %s", masterurl .. file, dest))
+        table.insert(installed[paqname].files, dest)
+      end
+      
+      io.write("adding to table...\n")
+      installed[paqname].repo = repo
+      installed[paqname].version = t.version
+      installed[paqname].author = authorname
+      installed[paqname].dependencies = t.dependencies
+    else
+      io.stderr:write("Could not parse package info!\n"..str.."\n")
+    end
+    
+  else
+    io.stderr:write("Could not find package info!\n")
+  end
+end
+
+function paq.save()
+  for k, v in pairs( installed ) do
+    if(not v.main and not v.repo) then
+      installed[k] = nil
+    end
+  end
+  
+  f = io.open(configpath .. "paqs.tb", "w")
+  outp = serialization.serialize(installed)
+  
+  f:write(outp)
+  f:flush()
+  f:close()
+end
+
+function paq.remove(paqname)
+  if installed[paqname] then
+    if installed[paqname].main then
+      fs.remove(installed[paqname].main)
+      
+      if installed[paqname].repo then
+        fs.remove(installed[paqname].repo)
+      end
+      
+      io.write(string.format("Package %s:%s was successfully removed!\n", installed[paqname].author, paqname))
+      installed[paqname] = nil
+    else
+      io.stderr:write("Could not find binary for package " .. paqname .. ", removing it from database.\n")
+      installed[paqname] = nil
+    end
+  else
+    io.write("Package " .. paqname .. " is not installed.")
+  end
+end
+
+return paq
--- src/paq.lua
+++ src/paq.lua
@@ -4,98 +4,10 @@
 
 local VERSION = "0.1.2"
 local binname = "paq"
-local configpath = "/etc/" .. binname .. "/"
-local librarypath = "/lib/" .. binname .. "/"
-local repositorypath = "/usr/" .. binname .. "/"
 
 local component = require("component")
-local fs = require("filesystem")
-local internet = require("internet")
+local paq = require("lib" .. binname)
 local shell = require("shell")
-local text = require("text")
-local serialization = require("serialization")
-local installed = {}
-
-local function usage()
-  io.write("Usage: " .. binname .. " command [argument]\n")
-  io.write("Commands:\n")
-  io.write(" installpaq\t\t\tinstall this tool to system\n")
-  io.write(" install user paqname\t\tinstall package <paqname> from Github user <user>\n")
-  io.write(" version\t\t\tprint " .. binname .. " version\n")
-end
-
-local function init()
-  if fs.exists(configpath .. "paqs.tb") then
-    local f = io.open(configpath .. "paqs.tb", "r")
-    local data = f:read("*all")
-    installed = serialization.unserialize(data) or {}
-    f:close()
-  elseif not fs.exists(configpath) then
-    fs.makeDirectory(configpath)
-  end
-end
-
-local function get(authorname, paqname)
-  authorname = authorname or "exploser"
-  
-  if(installed[authorname]) then
-    if(installed[authorname][paqname]) then
-      io.write(string.format("Package %s:%s is already installed! Use '%s delete %s %s' to remove it.\n", authorname, paqname, binname, authorname, paqname))
-      return
-    end
-  end
-  
-  local masterurl = "https://raw.githubusercontent.com/" .. authorname .. "/" .. paqname .. "/master/"
-  local infourl = masterurl .. "package.txt"
-  io.write(string.format("Retrieving %s...\n", infourl))
-  local result, response = pcall(internet.request, infourl)
-  if result then
-    local str = ""
-    for chunk in response do
-      str = str .. chunk
-    end
-    local t = serialization.unserialize(str)
-    local binurl = masterurl .. t["binname"]
-    local repo = repositorypath .. paqname .. "/"
-    io.write("Creating local repo directory " .. repo .. "\n")
-    fs.makeDirectory(repo)
-    local index = string.find(t["binname"], "/[^/]*$") or 0
-    local filename = string.sub(t["binname"], index + 1)
-    
-    if(fs.exists("/bin/" .. filename)) then
-      io.stderr:write(string.format("File /bin/%s already exists! Try removing it by hand. %s does not delete existing files if it didn't install them.\n", filename, binname))
-      return
-    end
-    
-    os.execute(string.format("wget %s /bin/%s", binurl, filename))
-    io.write("adding to table...\n")
-    installed[authorname] = {}
-    installed[authorname][paqname] = {}
-    installed[authorname][paqname].bin = "/bin/" .. filename
-    installed[authorname][paqname].repo = repo
-    installed[authorname][paqname].version = 0
-    
-  end
-end
-
-local function install()
-  get("exploser", "paq")
-end
-
-local function save()
-  for k, v in pairs( installed ) do
-    if(not v.bin and not v.repo) then
-      installed[k] = nil
-    end
-  end
-  
-  f = io.open(configpath .. "paqs.tb", "w")
-  outp = serialization.serialize(installed)
-  
-  f:write(outp)
-  f:flush()
-  f:close()
-end
 
 local function main(args)
   if not component.isAvailable("internet") then
@@ -103,53 +15,43 @@ local function main(args)
     return
   end
   
-  init()
+  paq.init()
   
   if #args < 1 then
-    usage()
+    paq.usage()
     return
   end
   
-  if args[1] == "installpaq" then
-    install()
-    
-  elseif args[1] == "install" then
-    get(args[2], args[3])
+  if args[1] == "install" then
+    if (#args == 2) then
+      paq.get(args[2])
+    elseif(#args == 3) then
+      paq.get(args[3], args[2])
+    else
+      paq.usage()
+    end
     
   elseif args[1] == "version" then
     io.write(VERSION)
     
   elseif args[1] == "remove" or args[1] == "delete" then
-    local authorname = args[2]
-    local paqname = args[3]
-    
-    if installed[authorname][paqname]["bin"] then
-      fs.remove(installed[authorname][paqname]["bin"])
-      installed[authorname][paqname]["bin"] = nil
-      io.write("Package " .. paqname .. " was successfully removed!")
-    else
-      io.write("Could not find binary for package " .. paqname .. "!")
+    paq.remove(args[2])
+       
+  elseif args[1] == "workdir" then
+    if(paq.installed[args[2]]) then
+      io.write(paq.installed[args[2]].repo)
     end
     
-    if installed[authorname][paqname]["repo"] then
-      fs.remove(installed[authorname][paqname]["repo"])
-      installed[authorname][paqname]["repo"] = nil
-    else
-      io.write("Could not find repository for package " .. paqname .. "!")
-    end
-    
-  elseif args[1] == "getdep" then
-    if(installed[args[2]]) then
-      io.write(installed[args[2]].repo)
-    end
   elseif args[1] == "list" then
-    for k, v in pairs( installed ) do
-      print(k)
+    for k, v in pairs(paq.installed) do
+      print(k.author .. ":" .. k)
     end
+  
+  elseif args[1] == "client" then
+    io.write(paq.librarypath .. binname .. "client.lua")
   end
   
-  
-  save()
+  paq.save()
     
 end
 
